d39e6b4f871363e4762bb6a73eafb55c4677cc76
ammustephy <ammustephy.as@gmail.com>
2024-03-23 11:23:15 +0530

    first commit

---
+++ bin/examm.dart
@@ -0,0 +1,5 @@
+import 'package:examm/examm.dart' as examm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${examm.calculate()}!');
+}
---
+++ test/examm_test.dart
@@ -0,0 +1,8 @@
+import 'package:examm/examm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
